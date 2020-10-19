8fca24ad23f34b473d6db11e48d7d04d1311a555
Despina Marianna Patronas <dpatrona@unix2.lt.ucsc.edu>
2020-10-18 17:42:35 -0700

    formatting from unix server because local doesnt recognize command...

--- asgn1/lrc.c
+++ asgn1/lrc.c
@@ -1,44 +1,37 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
-//variable declarations static scope
-typedef enum faceim {LEFT, RIGHT, CENTER, PASS} faces;
+// variable declarations static scope
+typedef enum faceim { LEFT, RIGHT, CENTER, PASS } faces;
 static faces die[] = {LEFT, RIGHT, CENTER, PASS, PASS, PASS};
-static const char *names[] = {"Happy", "Sleepy", "Sneezy", "Dopey", "Bashful", "Grumpy", "Doc", "Mirror Mirror", "Snow White", "Wicked Queen"};
+static const char *names[] = {
+    "Happy",  "Sleepy", "Sneezy",        "Dopey",      "Bashful",
+    "Grumpy", "Doc",    "Mirror Mirror", "Snow White", "Wicked Queen"};
 static int players, pot, pos, seed = 0;
 
-//returns the position to the left of the current player (pos)
-int left(int pos, int players) {
-  return ((pos + players - 1) % players);
+// returns the position to the left of the current player (pos)
+int left(int pos, int players) { return ((pos + players - 1) % players); }
 
-}
+// returns the position to the right of the current player (pos)
+int right(int pos, int players) { return ((pos + players + 1) % players); }
 
-//returns the position to the right of the current player (pos)
-int right(int pos, int players) {
-  return ((pos + players + 1) % players);
+// function to generate a new random number between 0 and 5
+int roll() { return rand() % 6; }
 
-}
-
-//function to generate a new random number between 0 and 5
-int roll() {
-  return rand() % 6;
-
-}
-
-//checks the players in the game 
+// checks the players in the game
 int checkplayers(int *pbalance, int numplayers) {
-  players = 0; //reset player count
+  players = 0; // reset player count
   for (int i = 0; i < numplayers; i++) {
     if (*pbalance > 0) {
-      players++; //count players with money
+      players++; // count players with money
     }
-      pbalance++;
+    pbalance++;
   }
   return players;
 }
 
-//Output / Input for variable assignment of seed and players
+// Output / Input for variable assignment of seed and players
 void start() {
   printf("Random seed:\n");
   scanf("%ud", &seed);
@@ -48,33 +41,34 @@ void start() {
 }
 
 int main(void) {
-  int temp, maxroll = 0; 
+  int temp, maxroll = 0;
   start();
-  //allocate and initialize players balance values to 3
-  int pbalance[10]; 
-  for ( int i = 0; i < players; i++) {
+  // allocate and initialize players balance values to 3
+  int pbalance[10];
+  for (int i = 0; i < players; i++) {
     pbalance[i] = 3;
   }
 
-  //win condition: be the last player with money
-  while (checkplayers (pbalance, players) > 1) {
+  // win condition: be the last player with money
+  while (checkplayers(pbalance, players) > 1) {
 
-    if (pbalance[pos] > 0 ) { // if current pos player is in the game
-      printf("%s rolls...", names[pos]); maxroll = 0;
-      
-      while ( (pbalance[pos] > 0) && (maxroll < 3) ) {
+    if (pbalance[pos] > 0) { // if current pos player is in the game
+      printf("%s rolls...", names[pos]);
+      maxroll = 0;
+
+      while ((pbalance[pos] > 0) && (maxroll < 3)) {
         temp = roll();
-        switch ( die[temp] ) {
-        case (0): //player lands on LEFT
+        switch (die[temp]) {
+        case (0): // player lands on LEFT
           printf(" gives $1 to %s", names[left(pos, players)]);
           pbalance[pos]--;
-          pbalance[left(pos,players)]++;
-          maxroll ++;
+          pbalance[left(pos, players)]++;
+          maxroll++;
           break;
-        case (1): //player lands on RIGHT
+        case (1): // player lands on RIGHT
           printf(" gives $1 to %s", names[right(pos, players)]);
           pbalance[pos]--;
-          pbalance[right(pos,players)]++;
+          pbalance[right(pos, players)]++;
           maxroll++;
           break;
         case (2): // player lands on CENTER
@@ -87,7 +81,8 @@ int main(void) {
           printf(" gets a pass");
           maxroll++;
           break;
-        default: printf("Bad case!");
+        default:
+          printf("Bad case!");
           break;
         }
       }
@@ -95,7 +90,7 @@ int main(void) {
       pos = right(pos, players);
     }
   }
-  printf("%s wins the $%d pot with $%d left in the bank!", names[pos], pot, pbalance[pos]);
+  printf("%s wins the $%d pot with $%d left in the bank!", names[pos], pot,
+         pbalance[pos]);
   return 0;
-
 }
