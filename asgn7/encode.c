35adfccecd25f563770fd6d64f7fa590c71c67ec
Despina Patronas <despinapatronas@Despinas-MacBook-Pro.local>
2020-12-13 23:12:30 -0800

    fixing the encode compression and file protection functions

--- asgn7/encode.c
+++ asgn7/encode.c
@@ -1,115 +1,119 @@
 #include "io.h"
-#include "trie.h"
 #include "word.h"
-#include <getopt.h>
+#include "trie.h"
 #include <math.h>
+#include <getopt.h>
 #include <stdbool.h>
 
-#include <fcntl.h>
-#include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 
-// extern stats
+//extern stats
 uint64_t uncompressed_bits;
 uint64_t compressed_bits;
 
 #define OPTIONS "vi:o:"
 
-int bit_length(int num) { return (log2(num) + 1); }
+int bit_length (int num) {
+	return (log2(num) + 1);
+}
 
-int main(int argc, char **argv) {
+int main (int argc, char ** argv) {
 
-  int c = 0;
-  bool stats = false;
+	int c = 0;
+	bool stats = false;
 
-  // default in / out file
-  int infile = STDIN_FILENO;
-  int outfile = STDOUT_FILENO;
+	//default in / out file
+	int infile = STDIN_FILENO;
+	int outfile = STDOUT_FILENO;
 
-  while ((c = getopt(argc, argv, OPTIONS)) != -1) {
-    switch (c) {
+	while ((c=getopt(argc, argv, OPTIONS)) != -1) {
+		switch (c) {
 
-    // display compression statistics
-    case 'v':
-      stats = true;
-      break;
+			//display compression statistics
+			case 'v':
+				stats = true;
+				break;
 
-    // specify input other than stdin
-    case 'i':
-      infile = open(optarg, O_RDONLY);
-      break;
+			//specify input other than stdin
+			case 'i':
+				infile = open(optarg, O_RDONLY);
+				break;
 
-    // specify output other than stout
-    case 'o':
-      outfile = open(optarg, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-      break;
-    }
-  }
+			//specify output other than stout
+			case 'o':
+				outfile = open(optarg, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+				break;
+		}
+	}
 
-  // make a fileheader struct contains the protection bits and magic num
-  FileHeader hd = {0, 0};
-  read_header(infile, &hd);
-  hd.magic = MAGIC;
+	// read input file protection
+	struct stat permissions;
+	fstat(infile, &permissions);
 
-  if (hd.magic != 0x8badbeef) {
-    exit(1);
-  }
+	//outfile emulates infile protection
+	FileHeader hd = {0,0};
+	hd.magic = MAGIC;
+	hd.protection = permissions.st_mode;
+	write_header(outfile, &hd);
 
-  // file protection
-  struct stat permissions;
-  fstat(infile, &permissions);
-  hd.protection = permissions.st_mode;
-  // outfile emulates infile protection
-  fchmod(outfile, hd.protection);
-  write_header(outfile, &hd);
+	//encoding based on provided pseudocode in asgn7 lab
+	TrieNode *root = trie_create();
+	TrieNode *curr_node = root;
+	TrieNode *prev_node = NULL;
+	uint8_t curr_sym = 0;
+	uint8_t prev_sym = 0;
+	uint16_t next_code = START_CODE;
 
-  // encoding based on provided pseudocode in asgn7 lab
-  TrieNode *root = trie_create();
-  TrieNode *curr_node = root;
-  TrieNode *prev_node = NULL;
-  uint8_t curr_sym = 0;
-  uint8_t prev_sym = 0;
-  uint16_t next_code = START_CODE; // START_CODE = 2
+	while (read_sym(infile, &curr_sym)) {
 
-  while (read_sym(infile, &curr_sym)) {
+		TrieNode * next_node = trie_step(curr_node, curr_sym);
+		
+		if (next_node) {
+			prev_node = curr_node;
+			curr_node = next_node;
+		}
+		else {
+			buffer_pair(outfile, curr_node->code, curr_sym, bit_length(next_code));
 
-    TrieNode *next_node = trie_step(curr_node, curr_sym);
+			curr_node->children[curr_sym] = trie_node_create(next_code);
+			curr_node = root;
 
-    if (next_node) {
-      prev_node = curr_node;
-      curr_node = next_node;
-    } else {
-      buffer_pair(outfile, curr_node->code, curr_sym, bit_length(next_code));
-      curr_node->children[curr_sym] = trie_node_create(next_code);
-      curr_node = root;
-      next_code++;
+			next_code++;
+			if (next_code == MAX_CODE) {
+				trie_reset(root);
+				curr_node = root;
+				next_code = START_CODE;
+			}
 
-      if (next_code == MAX_CODE) {
-        trie_reset(root);
-        curr_node = root;
-        next_code = START_CODE;
-      }
-      prev_sym = curr_sym;
-    }
-    if (curr_node != root) {
-      buffer_pair(outfile, STOP_CODE, 0, bit_length(next_code));
-      next_code = (next_code + 1) % MAX_CODE;
-    }
-    buffer_pair(outfile, STOP_CODE, 0, bit_length(next_code));
-    flush_pairs(outfile);
-  }
+			prev_sym = curr_sym;
+		}
+	}
 
-  trie_delete(root);
-  close(infile);
-  close(outfile);
+	if (curr_node != root) {
+		buffer_pair(outfile, prev_node->code, prev_sym, bit_length(next_code));
+		next_code = (next_code + 1) % MAX_CODE;
+	}
 
-  if (stats) {
-    printf("Compressed file size: %lu bytes", compressed_bits);
-    printf("Uncompressed file size: %lu bytes", uncompressed_bits);
-    printf("Compression ratio: %lu%%",
-           (100 * (1 - (compressed_bits / uncompressed_bits))));
-  }
+	buffer_pair(outfile, STOP_CODE, 0, bit_length(next_code));
+	flush_pairs(outfile);
 
-  return 0;
+	trie_delete(root);
+
+	if (stats) {
+		printf("Compressed file size: %llu bits\n", compressed_bits);
+		printf("Uncompressed file size: %llu bits\n", uncompressed_bits);
+
+		long long ratio = 0;
+		ratio = (long long)(100 * (double)uncompressed_bits/compressed_bits);
+
+		printf("Compression ratio: %llu%%\n", ratio);
+	}
+
+	close(infile);
+	close(outfile);
+
+	return 0;
 }
